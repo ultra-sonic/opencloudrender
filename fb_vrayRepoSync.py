9e9bba31a8738ce95001638420d49acd1b53fdbf
Oliver Markowski <oliver@fullblownimages.com>
2015-02-15 22:20:29 +0100

    vray repo push and pull

--- fb_vrayRepoSync.py
+++ fb_vrayRepoSync.py
@@ -3,7 +3,8 @@ from fb_s3IO import upload_files,get_files_in_directory
 bucket_name = 'vray_repo'
 
 def push( releaseType="*" , build="*" , host_app="*" ):
-    allFiles = get_files_in_directory( "/opt/vray" )
+    sourceDir = "/opt/vray"
+    allFiles = get_files_in_directory( sourceDir )
     upload_files( bucket_name , sourceDir , allFiles )
 
 def pull():
