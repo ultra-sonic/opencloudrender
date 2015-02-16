8cda6d806e647f7d340f4908d7d018f1d92e6c61
Oliver Markowski <oliver@fullblownimages.com>
2015-02-16 01:32:06 +0100

    testing upload and md5 check

--- fb_vrayRepoSync.py
+++ fb_vrayRepoSync.py
@@ -4,7 +4,7 @@ from fb_s3IO import upload_files,get_files_in_directory
 bucket_name = 'vray-repo'
 
 # build="31003" or build="24002"
-def push( releaseType="official" , build="" , host_app="" ):
+def push( releaseType="official" , build="" , operating_system="Linux" , host_app="" ):
     """
 
     :rtype : int
@@ -12,7 +12,7 @@ def push( releaseType="official" , build="" , host_app="" ):
     debugCount=0
     sourcePrefix = '/opt/vray'
     for (sourceDir, dirname, filenames) in os.walk( sourcePrefix ):
-        if filenames!=[] and sourceDir.find( releaseType )>-1 and sourceDir.find( build )>-1 and sourceDir.find( host_app )>-1:
+        if filenames!=[] and sourceDir.find( releaseType )>-1 and sourceDir.find( build )>-1 and sourceDir.find( host_app )>-1 and sourceDir.find( operating_system )>-1:
             if debugCount < 2:
                 print( '{0} ::: {1} ::: {2}'.format( sourceDir, dirname, filenames ) )
                 upload_files( bucket_name , sourceDir , filenames , sourcePrefix )
