2b83c4270f9bfd80b5dea865e397fd2041f9298d
Oliver Markowski <oliver@fullblownimages.com>
2015-03-30 23:50:31 +0200

    working windows launcher

---
+++ bin/ocrSubmitUI.bat
@@ -0,0 +1,9 @@
+rem # minimal afanasy setup
+set CGRU_LOCATION=%~dp0/../cgru
+set AF_ROOT=%CGRU_LOCATION%/afanasy
+set PYTHONPATH=%~dp0/..;%CGRU_LOCATION%/lib/python;%AF_ROOT%/python;$PYTHONPATH
+echo PYTHONPATH: %PYTHONPATH%
+rem # afanasy done
+
+%~dp0/ocrSubmitUI.py
+pause
